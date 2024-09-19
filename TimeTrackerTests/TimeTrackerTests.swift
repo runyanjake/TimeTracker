6b2ef21500302300964332fe1baf6d545fdff1c9
Jake Runyan <runyanjake@gmail.com>
2024-09-19 00:44:57 -0700

    Basic IOS app template

---
+++ TimeTrackerTests/TimeTrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  TimeTrackerTests.swift
+//  TimeTrackerTests
+//
+//  Created by Jake Runyan on 9/19/24.
+//
+
+import Testing
+
+struct TimeTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
